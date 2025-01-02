3c9f9c58acc000b2655582730aa77ba9f7246fb9
Anmol Kumar <110680449+anmol2517@users.noreply.github.com>
2025-01-02 13:59:51 +0530

    Update setupTests.js

--- web-tier/src/setupTests.js
+++ web-tier/src/setupTests.js
@@ -1,7 +1,7 @@
 // jest-dom adds custom jest matches for asserting on DOM nodes
 
 // expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// https://github.com/testing-library/jest-dom
 
 
 import '@testing-library/jest-dom';
